4a6e0934affcf80931632b8f51ef013e551d4c9c
Pablo Cuevas <cuevasgpablo@gmail.com>
2023-03-04 22:58:11 -0300

    feat: default layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+   <div
+      class="h-screen flex flex-col bg-orange-50 dark:bg-stone-800 transition-colors ease-linear text-stone-900 dark:text-orange-50"
+   >
+      <TheHeader />
+      <main class="flex-auto m-2">
+         <NuxtPage />
+      </main>
+   </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+useHead({
+   title: "RUT Random",
+   link: [{ rel: "icon", href: "/favicon.png" }],
+});
+</script>
+
 <template>
-    <h1>HOLA MUNDO</h1>
+   <h1>HOLA MUNDO</h1>
 </template>
