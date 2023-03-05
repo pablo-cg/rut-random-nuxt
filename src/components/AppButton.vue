c60ce394d922d4acd1e6bb4018a849ad5acb23f9
Pablo Cuevas <cuevasgpablo@gmail.com>
2023-03-04 22:55:36 -0300

    feat: add icon and app button

---
+++ src/components/AppButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface ButtonProps {
+   label: string;
+   disabled?: boolean;
+}
+
+defineProps<ButtonProps>();
+</script>
+<template>
+   <button
+      :class="[
+         'transition ease-linear py-2 px-4 rounded-full focus:outline-none',
+         $attrs.class,
+      ]"
+      :disabled="disabled"
+   >
+      {{ label }}
+   </button>
+</template>
---
+++ src/components/IconButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+type ButtonIconProps = {
+   icon: string;
+};
+
+defineProps<ButtonIconProps>();
+</script>
+
+<template>
+   <button
+      class="p-2 shadow-md rounded-full flex items-center text-2xl dark:hover:bg-stone-700 hover:bg-stone-200 active:shadow-none"
+   >
+      <Icon :name="icon" />
+   </button>
+</template>
