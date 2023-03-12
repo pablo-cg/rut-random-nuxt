79e48cb192828da56c89482d754a905c2ada48d5
Pablo Cuevas <cuevasgpablo@gmail.com>
2023-03-12 17:37:32 -0300

    style: fix background color on mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
    <div
-      class="h-screen flex flex-col bg-orange-50 dark:bg-stone-800 transition-colors ease-linear text-stone-900 dark:text-orange-50"
+      class="h-screen flex flex-col"
    >
       <TheHeader />
       <main class="flex-auto px-4 w-full lg:max-w-7xl lg:mx-auto">
--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@ useHead({
 </script>
 
 <template>
-   <main class="pt-3 gap-10 grid grid-cols-1 lg:gap-0 lg:grid-cols-2 lg:content-center lg:h-full">
+   <main class="py-3 gap-10 grid grid-cols-1 lg:gap-0 lg:grid-cols-2 lg:content-center lg:h-full">
       <GeneratorRut />
       <GeneratorEmail />
    </main>
