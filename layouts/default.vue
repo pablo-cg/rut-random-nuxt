aa218007068b018086b12a252175eb2f322dd739
Pablo Cuevas <cuevasgpablo@gmail.com>
2023-03-12 17:18:53 -0300

    style: add responsiveness to layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
       class="h-screen flex flex-col bg-orange-50 dark:bg-stone-800 transition-colors ease-linear text-stone-900 dark:text-orange-50"
    >
       <TheHeader />
-      <main class="flex-auto m-2">
+      <main class="flex-auto px-4 w-full lg:max-w-7xl lg:mx-auto">
          <NuxtPage />
       </main>
    </div>
