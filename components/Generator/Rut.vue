aa85eded8e4767f597a94ed9e09c0416ea4ba585
Pablo Cuevas <pcuevas@pegasus.cl>
2023-04-27 14:47:40 -0400

    feat: post total ruts al generar una nueva lista

--- components/Generator/Rut.vue
+++ components/Generator/Rut.vue
@@ -1,33 +1,46 @@
 <script setup lang="ts">
-import { Item } from "~~/types";
+import { useRutStore } from '~~/stores/rut'
+import { Item } from '~~/types'
 
-const { Rut } = useRut();
+const { Rut } = useRut()
+const { postRutsGenerated } = useRutStore()
 
-const rutList = ref<Item[]>([]);
+const rutList = ref<Item[]>([])
+const totalRutsGenerated = ref(0)
 
-function generateRandomRutList() {
-   rutList.value = [];
+const debounceRutPost = useDebounceFn((totalRuts: number) => {
+  postRutsGenerated(totalRuts).finally(() => {
+    totalRutsGenerated.value = 0
+  })
+}, 1500)
 
-   for (let i = 0; i < 10; i++) {
-      rutList.value.push(new Rut());
-   }
+async function generateRandomRutList() {
+  rutList.value = []
+
+  for (let i = 0; i < 10; i++) {
+    rutList.value.push(new Rut())
+  }
+
+  totalRutsGenerated.value += rutList.value.length
+  
+  await debounceRutPost(totalRutsGenerated.value)
 }
 
-onMounted(() => {
-   generateRandomRutList();
-});
+onMounted(async () => {
+  await generateRandomRutList()
+})
 </script>
 
 <template>
-   <main class="w-full flex flex-col gap-3">
-      <GeneratorBanner
-         title="RUT Random"
-         logo="/identity.png"
-         btn-label="Generar Ruts"
-         @generate="generateRandomRutList"
-      >
-         Genera una lista con 10 RUT aleatorios
-      </GeneratorBanner>
-      <GeneratorList :items="rutList" />
-   </main>
+  <main class="w-full flex flex-col gap-3">
+    <GeneratorBanner
+      title="RUT Random"
+      logo="/identity.png"
+      btn-label="Generar Ruts"
+      @generate="generateRandomRutList"
+    >
+      Genera una lista con 10 RUT aleatorios
+    </GeneratorBanner>
+    <GeneratorList :items="rutList" />
+  </main>
 </template>
