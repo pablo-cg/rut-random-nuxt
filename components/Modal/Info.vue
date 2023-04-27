3a029f8105a89151a4f030ed6cc0d85352b944ce
Pablo Cuevas <pcuevas@pegasus.cl>
2023-04-27 16:11:02 -0400

    feat: estadisticas en modal info

--- components/Modal/Info.vue
+++ components/Modal/Info.vue
@@ -1,99 +1,128 @@
 <script setup lang="ts">
-import { useOptionsStore } from "@/stores/options";
-import { DialogTitle } from "@headlessui/vue";
+import { useOptionsStore } from '@/stores/options'
+import { DialogTitle } from '@headlessui/vue'
+import { storeToRefs } from 'pinia'
+import { useEmailStore } from '~~/stores/email'
+import { useRutStore } from '~~/stores/rut'
 
 type InfoModalProps = {
-   isOpen: boolean;
-};
+  isOpen: boolean
+  loading: boolean
+}
 
 type InfoModalEmits = {
-   (e: "close"): void;
-};
+  (e: 'close'): void
+}
 
-defineProps<InfoModalProps>();
-const emit = defineEmits<InfoModalEmits>();
+defineProps<InfoModalProps>()
+const emit = defineEmits<InfoModalEmits>()
 
-const optionsStore = useOptionsStore();
+const optionsStore = useOptionsStore()
 
-const minimumRut = computed(() => optionsStore.minRut.toLocaleString());
-const maximumRut = computed(() => optionsStore.maxRut.toLocaleString());
+const rutStore = useRutStore()
+const emailStore = useEmailStore()
+
+const { totalRutsCopied, totalRutsGenerated } = storeToRefs(rutStore)
+const { totalEmailsCopied, totalEmailsGenerated } = storeToRefs(emailStore)
+
+const minimumRut = computed(() => optionsStore.minRut.toLocaleString())
+const maximumRut = computed(() => optionsStore.maxRut.toLocaleString())
 
 const frameworks = ref([
-   { url: "https://vuejs.org/", icon: "IconVue" },
-   { url: "https://nuxt.com/", icon: "IconNuxt" },
-   { url: "https://headlessui.com/", icon: "IconHeadlessUI" },
-   { url: "https://tailwindcss.com/", icon: "IconTailwind" },
-]);
+  { url: 'https://vuejs.org/', icon: 'IconVue' },
+  { url: 'https://nuxt.com/', icon: 'IconNuxt' },
+  { url: 'https://headlessui.com/', icon: 'IconHeadlessUI' },
+  { url: 'https://tailwindcss.com/', icon: 'IconTailwind' }
+])
 
 const socialNetworks = ref([
-   {
-      url: "https://github.com/pablo-cg",
-      icon: "ion:logo-github",
-      username: "pablo-cg",
-   },
-   {
-      url: "https://www.instagram.com/pablocg.uwu/?hl=es-la",
-      icon: "IconInstagram",
-      username: "pablocg.uwu",
-   },
-]);
+  {
+    url: 'https://github.com/pablo-cg',
+    icon: 'ion:logo-github',
+    username: 'pablo-cg'
+  },
+  {
+    url: 'https://www.instagram.com/pablocg.uwu/?hl=es-la',
+    icon: 'IconInstagram',
+    username: 'pablocg.uwu'
+  }
+])
 </script>
 
 <template>
-   <ModalWrapper :show="isOpen" @close="emit('close')">
-      <DialogTitle
-         as="h3"
-         class="px-2 mb-4 text-lg font-medium leading-6 text-stone-900 dark:text-orange-50 flex justify-between items-center"
+  <ModalWrapper :show="isOpen" @close="emit('close')">
+    <DialogTitle
+      as="h3"
+      class="px-2 mb-4 text-lg font-medium leading-6 text-stone-900 dark:text-orange-50 flex justify-between items-center"
+    >
+      <span>Acerca de</span>
+      <Icon name="mdi:account-question" class="text-2xl" />
+    </DialogTitle>
+    <main
+      class="px-2 grid grid-cols-1 lg:grid-cols-2 gap-4 text-stone-900 dark:text-orange-50"
+    >
+      <section
+        class="border col-span-full border-stone-300 dark:border-stone-600 rounded-lg p-3"
       >
-         <span>Acerca de</span>
-         <Icon name="mdi:account-question" class="text-2xl" />
-      </DialogTitle>
-      <main
-         class="px-2 grid grid-cols-1 lg:grid-cols-2 gap-4 text-stone-900 dark:text-orange-50"
+        <div
+          class="flex justify-between items-center text-2xl font-bold mb-3 px-2"
+        >
+          <h1>Configuración Actual</h1>
+          <Icon name="clarity:settings-solid" />
+        </div>
+        <ul class="px-2 flex flex-col gap-1">
+          <li>RUT Mínimo: {{ minimumRut }}</li>
+          <li>RUT Máximo: {{ maximumRut }}</li>
+        </ul>
+      </section>
+      <section
+        class="border col-span-full border-stone-300 dark:border-stone-600 rounded-lg p-3"
       >
-         <section
-            class="border col-span-full border-stone-300 dark:border-stone-600 rounded-lg p-3"
-         >
-            <div
-               class="flex justify-between items-center text-2xl font-bold mb-3 px-2"
-            >
-               <h1>Configuración Actual</h1>
-               <Icon name="clarity:settings-solid" />
-            </div>
-            <ul class="px-2 flex flex-col gap-1">
-               <li>RUT Mínimo: {{ minimumRut }}</li>
-               <li>RUT Máximo: {{ maximumRut }}</li>
-            </ul>
-         </section>
-         <section
-            class="border col-span-full border-stone-300 dark:border-stone-600 rounded-lg p-3"
-         >
-            <div
-               class="flex justify-between items-center text-2xl font-bold mb-3 px-2"
-            >
-               <h1>RUT Random</h1>
-               <Icon name="clarity:info-standard-solid" />
-            </div>
-            <ul class="px-2 flex flex-col gap-1">
-               <li v-for="social in socialNetworks" :key="social.url">
-                  <NuxtLink :to="social.url" target="_blank">
-                     <Icon :name="social.icon" class="text-2xl" />
-                     {{ social.username }}
-                  </NuxtLink>
-               </li>
-            </ul>
-         </section>
-         <section
-            class="border col-span-full lg:col-span-2 border-stone-300 dark:border-stone-600 rounded-lg p-3"
-         >
-            <ul class="px-2 flex gap-4 justify-center">
-               <li v-for="framework in frameworks" :key="framework.url">
-                  <NuxtLink :to="framework.url" target="_blank">
-                     <Icon :name="framework.icon" class="text-2xl" />
-                  </NuxtLink>
-               </li>
-            </ul>
-         </section>
-      </main>
-   </ModalWrapper>
+        <div
+          class="flex justify-between items-center text-2xl font-bold mb-3 px-2"
+        >
+          <h1>Estadisticas</h1>
+          <Icon name="clarity:analytics-solid" />
+        </div>
+        <div class="px-2 flex flex-col items-center gap-1" v-if="loading">
+          <Icon name="line-md:loading-twotone-loop" size="3rem" />
+        </div>
+        <ul class="px-2 flex flex-col gap-1" v-else>
+          <li>RUTs Generados: {{ totalRutsGenerated }}</li>
+          <li>RUTs Copiados: {{ totalRutsCopied }}</li>
+          <li>Emails Generados: {{ totalEmailsGenerated }}</li>
+          <li>Emails Copiados: {{ totalEmailsCopied }}</li>
+        </ul>
+      </section>
+      <section
+        class="border col-span-full border-stone-300 dark:border-stone-600 rounded-lg p-3"
+      >
+        <div
+          class="flex justify-between items-center text-2xl font-bold mb-3 px-2"
+        >
+          <h1>RUT Random</h1>
+          <Icon name="clarity:info-standard-solid" />
+        </div>
+        <ul class="px-2 flex flex-col gap-1">
+          <li v-for="social in socialNetworks" :key="social.url">
+            <NuxtLink :to="social.url" target="_blank">
+              <Icon :name="social.icon" class="text-2xl" />
+              {{ social.username }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </section>
+      <section
+        class="border col-span-full lg:col-span-2 border-stone-300 dark:border-stone-600 rounded-lg p-3"
+      >
+        <ul class="px-2 flex gap-4 justify-center">
+          <li v-for="framework in frameworks" :key="framework.url">
+            <NuxtLink :to="framework.url" target="_blank">
+              <Icon :name="framework.icon" class="text-2xl" />
+            </NuxtLink>
+          </li>
+        </ul>
+      </section>
+    </main>
+  </ModalWrapper>
 </template>
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,29 +1,55 @@
 <script setup lang="ts">
-const isOpenInfoModal = ref(false);
-const isOpenConfigModal = ref(false);
+import { useEmailStore } from '~~/stores/email'
+import { useRutStore } from '~~/stores/rut'
 
-const toggleInfoModal = useToggle(isOpenInfoModal);
-const toggleConfigModal = useToggle(isOpenConfigModal);
+const { getRutsCopied, getRutsGenerated } = useRutStore()
+const { getEmailsCopied, getEmailsGenerated } = useEmailStore()
+
+const isOpenInfoModal = ref(false)
+const isOpenConfigModal = ref(false)
+
+const toggleInfoModal = useToggle(isOpenInfoModal)
+const toggleConfigModal = useToggle(isOpenConfigModal)
+
+const isLoadingModal = ref(false)
+
+async function getAnalytics() {
+  isLoadingModal.value = true
+  await getRutsCopied()
+  await getRutsGenerated()
+  await getEmailsCopied()
+  await getEmailsGenerated()
+  isLoadingModal.value = false
+}
+
+async function loadInfoModal() {
+  toggleInfoModal()
+  await getAnalytics()
+}
 
 function closeInfoModal() {
-   isOpenInfoModal.value = false;
+  isOpenInfoModal.value = false
 }
 
 function closeConfigModal() {
-   isOpenConfigModal.value = false;
+  isOpenConfigModal.value = false
 }
 </script>
 <template>
-   <header class="h-12 flex justify-between items-center px-4">
-      <section class="flex gap-4 items-center">
-         <AppIconButton
-            icon="icon-park-outline:config"
-            @click="toggleConfigModal()"
-         />
-         <AppIconButton icon="icon-park-outline:info" @click="toggleInfoModal()" />
-      </section>
-      <TheThemeSwitch />
-   </header>
-   <ModalInfo :is-open="isOpenInfoModal" @close="closeInfoModal" />
-   <ModalConfig :is-open="isOpenConfigModal" @close="closeConfigModal" />
+  <header class="h-12 flex justify-between items-center px-4">
+    <section class="flex gap-4 items-center">
+      <AppIconButton
+        icon="icon-park-outline:config"
+        @click="toggleConfigModal()"
+      />
+      <AppIconButton icon="icon-park-outline:info" @click="loadInfoModal" />
+    </section>
+    <TheThemeSwitch />
+  </header>
+  <ModalInfo
+    :is-open="isOpenInfoModal"
+    @close="closeInfoModal"
+    :loading="isLoadingModal"
+  />
+  <ModalConfig :is-open="isOpenConfigModal" @close="closeConfigModal" />
 </template>
