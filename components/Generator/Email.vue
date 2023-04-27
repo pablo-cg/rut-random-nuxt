e362eefffa1a3396983bae6ef358604df212c6e5
Pablo Cuevas <pcuevas@pegasus.cl>
2023-04-27 15:09:15 -0400

    refactor: copy email desde emit

--- components/Generator/Email.vue
+++ components/Generator/Email.vue
@@ -1,33 +1,38 @@
 <script setup lang="ts">
-import { Item } from "~~/types";
+import { Item } from '~~/types'
 
-const { Email } = useEmail();
+const { Email } = useEmail()
 
-const emailList = ref<Item[]>([]);
+const emailList = ref<Item[]>([])
 
 function generateRandomEmailList() {
-   emailList.value = [];
+  emailList.value = []
 
-   for (let i = 0; i < 10; i++) {
-      emailList.value.push(new Email());
-   }
+  for (let i = 0; i < 10; i++) {
+    emailList.value.push(new Email())
+  }
+}
+
+function copyEmail(item: Item) {
+  item.isCopied = true
+  navigator.clipboard.writeText(item.value)
 }
 
 onMounted(() => {
-   generateRandomEmailList();
-});
+  generateRandomEmailList()
+})
 </script>
 
 <template>
-   <main class="w-full flex flex-col gap-3">
-      <GeneratorBanner
-         title="Email Random"
-         logo="/email.png"
-         btn-label="Generar Emails"
-         @generate="generateRandomEmailList"
-      >
-         Genera una lista con 10 email aleatorios
-      </GeneratorBanner>
-      <GeneratorList :items="emailList" />
-   </main>
+  <main class="w-full flex flex-col gap-3">
+    <GeneratorBanner
+      title="Email Random"
+      logo="/email.png"
+      btn-label="Generar Emails"
+      @generate="generateRandomEmailList"
+    >
+      Genera una lista con 10 email aleatorios
+    </GeneratorBanner>
+    <GeneratorList :items="emailList" @copy-item="copyEmail" />
+  </main>
 </template>
